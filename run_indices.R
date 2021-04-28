77117448be8262be6b263e1590ad9a328f87460e
Bob Kubinec <bobkubinec@gmail.com>
2021-04-28 14:06:53 +0400

    redo hr index

--- run_indices.R
+++ run_indices.R
@@ -131,6 +131,7 @@ restrict_list <- switch(model_type,
 if(model_type=="hr") {
   
   # convert health resources to per capita
+  # need to de-scale oxford first
   
   wb_pop_country <- read_csv("data/wb_country_pop.csv") %>% 
     select(country="Country Name",
@@ -165,15 +166,20 @@ if(model_type=="hr") {
                           `Slovak Republic`="Slovakia",
                           `Syrian Arab Republic`="Syria",
                           `Yemen, Rep.`="Yemen")) %>% 
-    bind_rows(tibble(country=c("Taiwan","Vatican","Macau","Northern Cyprus"),
+    bind_rows(tibble(country=c("Taiwan","Vatican","Macau","Northern Cyprus","Eritrea"),
                      country_pop=c(23816775,
                                    825,
                                    640445,
-                                   326000)))
+                                   326000,
+                                   3214000))) %>% 
+    filter(!is.na(country_pop))
   
   index_long <- left_join(index_long,wb_pop_country,by="country") %>% 
+    group_by(country,item) %>% 
+    arrange(country,item,date_policy) %>% 
+    mutate(pop_out=ifelse(item=="ox_health_invest",cumsum(var),pop_out)) %>% 
     ungroup %>% 
-    mutate(pop_out=ifelse(item=="ox_health_invest",pop_out/scale(country_pop),pop_out))
+    mutate(pop_out=ifelse(item=="ox_health_invest",as.numeric(scale(pop_out/country_pop)),pop_out))
   
 }
 
@@ -181,9 +187,6 @@ if(model_type=="hr") {
   
 to_make <- index_long %>% 
   filter(item %in% filter_list) %>% 
-  group_by(country,item) %>% 
-  arrange(country,item,date_policy) %>% 
-  mutate(pop_out=ifelse(item=="ox_health_invest",cumsum(pop_out),pop_out)) %>% 
   group_by(item) %>% 
   mutate(model_id=case_when(item=="ox_health_invest"~9,
                             model_type=="sd" & grepl(x=item,pattern="ox")~5,
