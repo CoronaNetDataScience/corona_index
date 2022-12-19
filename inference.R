aaa3763d0cf8345cf9379eaf005717b2e5080806
Bob Kubinec <bobkubinec@gmail.com>
2022-12-19 10:11:47 +0400

    run with no correlation

--- inference.R
+++ inference.R
@@ -358,14 +358,14 @@ if(run_mod) {
     contact_mod <- contact_mod_code$sample(data=contact_mod_data,
                                            seed=638825,
                                            refresh=100,
-                                           chains=1,iter_warmup=500,
+                                           chains=4,iter_warmup=500,
                                            iter_sampling=500,
                                            max_treedepth=12,
                                            threads_per_chain=parallel::detectCores())
     
     contact_mod_samp <- contact_mod$draws(variables=c("b","bsp"))
     
-    saveRDS(contact_mod_samp, "/scratch/rmk7/coronanet/contact_mod_noimpute_nocor.rds")
+    saveRDS(contact_mod, "/scratch/rmk7/coronanet/contact_mod_noimpute_nocor.rds")
     
     
   }
